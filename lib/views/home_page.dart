37532015d0d2df46cd955cda20de859554c79e45
Mimimi-d <mimimi.dev.1202@gmail.com>
2022-08-25 16:03:02 +0900

    add: googleMapの表示

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -9,6 +10,34 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('TabiMap'),
       ),
+      body: MapSample(),
+    );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  const MapSample({Key? key}) : super(key: key);
+
+  @override
+  State<MapSample> createState() => _MapSampleState();
+}
+
+class _MapSampleState extends State<MapSample> {
+  late GoogleMapController _controller;
+
+  //初期位置
+  final CameraPosition _kGooglePlex = const CameraPosition(
+    target: LatLng(43.0686606, 141.3485613),
+    zoom: 14,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: _kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller = controller;
+      },
     );
   }
 }
