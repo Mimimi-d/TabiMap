930ad7f02d42e2e5c2ea291cc05fc6442173896c
Mimimi-d <mimimi.dev.1202@gmail.com>
2022-09-02 14:17:29 +0900

    update: packageインストールとディレクトリ構成変更

--- lib/provider/router.dart
+++ lib/provider/router.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import 'views/bottom_nav.dart';
-import 'views/home_page.dart';
-import 'views/home_page1.dart';
+import '../presentation/bottom_nav.dart';
+import '../presentation/home_page.dart';
+import '../presentation/home_page1.dart';
 
 final routerProvider = Provider(
   (ref) => GoRouter(
