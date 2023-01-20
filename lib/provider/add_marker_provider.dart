f64298b3c954b4ac3111e17ac5f4dfcbf57064c4
Mimimi-d <mimimi.dev.1202@gmail.com>
2023-01-21 01:33:11 +0900

    clean: remove unused import

--- lib/provider/add_marker_provider.dart
+++ lib/provider/add_marker_provider.dart
@@ -1,7 +1,6 @@
 //titleのcontrollerをStateProviderで定義する
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import 'package:tabimap/repository/marker_repository.dart';
 
